b1dad19f139a6054fa06614e233506d09a717fb7
Nikola Gospodinov <nikola96gospodinov@gmail.com>
2019-04-15 12:12:50 +0100

    Updates

---
+++ precache-manifest.eb0e5d1d4d0a7863016488b6b6895f0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2fdeba71c6a1e33e314",
+    "url": "/UdemyProject/static/css/main.20bc59d9.chunk.css"
+  },
+  {
+    "revision": "e2fdeba71c6a1e33e314",
+    "url": "/UdemyProject/static/js/main.006c320d.chunk.js"
+  },
+  {
+    "revision": "a2c813c54d61e225eab7",
+    "url": "/UdemyProject/static/js/runtime~main.14ad8a54.js"
+  },
+  {
+    "revision": "a553f21f231c793f13b7",
+    "url": "/UdemyProject/static/js/2.91b98516.chunk.js"
+  },
+  {
+    "revision": "1a7a3cdb2ce6eb575f09708832ca9e98",
+    "url": "/UdemyProject/index.html"
+  }
+];
